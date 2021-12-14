d6b29e121d071fe5f8d417b49467488d5ace5765
Shyam <shyamasundars43@gmail.com>
2021-12-14 16:55:47 +0530

    config table name on environment

--- backend/environment/env.development.js
+++ backend/environment/env.development.js
@@ -2,5 +2,10 @@ const sqlCred = require("../cred/mysqlCred");
 
 
 module.exports = {
-    dbConnection : sqlCred.dbConnect
+    dbConnection : sqlCred.dbConnect,
+    orderTable : 'order',
+    paymentTable : 'payment',
+    productsTable : 'products',
+    usersTable: 'users',
+    walleteTable : 'wallete'
 }
---
+++ backend/models/products.model.js
@@ -0,0 +1,19 @@
+const env = require("../cred/env");
+
+
+
+exports.insertProductData = (productInfo) => {
+
+
+    var q = "INSERT INTO customers (name, address) VALUES ('@333', 'Highway 37dfgh')";
+   
+    env.dbConnection.query(q, (err, result) => {
+        if (err) {
+            console.log("error on insert ==>", err);
+        } else {
+            console.log("Insert Success ==>",JSON.stringify(result));
+        }
+    })
+
+
+}
